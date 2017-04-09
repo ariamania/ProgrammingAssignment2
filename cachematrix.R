abd01a990c872a4f9484f44ee9e5983ce7410a0f
ariamania <aria.mania@gmail.com>
2017-04-10 00:42:37 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can
+## cashe its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	iv <- NULL
+	set <- function(y){
+		x <<- y
+		iv <<- NULL 
+	}
+	get <- function() x 
+	setinverse <- function(solve) iv <<- solve
+	getinverse <- function() iv 
+	list(
+		set = set, get = get, 
+	     	setinverse = setinverse
+	     	getinverse = getinverse
+		)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## reurned by the makeCasheMatrix function. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	iv <- x$getinverse()
+	if(!is.null(iv)) {
+		message("getting cashed data")
+		return(iv)
+	}
+	data <- x$get()
+	iv <- solve(data, ...)
+	x$setinverse(iv)
+	iv
 }
